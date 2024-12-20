d0bdf80539d0064abd33476a868a4193b668ece6
kirinokirino <helloworldagain2@gmail.com>
2024-12-20 14:11:44 +0100

    ls this and parent dirs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use crossterm::event::{poll, read, Event, KeyCode, KeyModifiers};
+use walkdir::WalkDir;
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
@@ -12,6 +13,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 struct App {
     starting_directory: PathBuf,
     current_directory: PathBuf,
+
+    current_directory_contents: Vec<String>,
+    parent_directory_contents: Vec<String>,
+
     should_run: bool,
 
     debug_messages: Vec<String>,
@@ -25,6 +30,10 @@ impl App {
         Ok(Self {
             starting_directory,
             current_directory,
+
+            current_directory_contents: Vec::new(),
+            parent_directory_contents: Vec::new(),
+
             should_run: true,
 
             debug_messages: Vec::new(),
@@ -65,15 +74,45 @@ impl App {
 
         Ok(())
     }
-    fn update(&mut self) {}
+    fn update(&mut self) {
+        self.current_directory_contents = directory_contents(&self.current_directory);
+        self.parent_directory_contents =
+            directory_contents(&self.parent_directory().unwrap_or("\\".into()));
+    }
     fn display(&self) {
+        let empty = String::new();
         print!("{CLEAR}");
         self.show_breadcrumbs();
 
+        let max_lines = self
+            .current_directory_contents
+            .len()
+            .max(self.parent_directory_contents.len());
+
+        for line in 0..max_lines {
+            let current_item = self.current_directory_contents.get(line).unwrap_or(&empty);
+
+            let parent_item = self.parent_directory_contents.get(line).unwrap_or(&empty);
+
+            println!("{parent_item} | {current_item}\r");
+        }
+
+        println!("\r");
         for line in &self.debug_messages {
             println!("{}\r", line);
         }
     }
+
+    // Update
+
+    fn parent_directory(&self) -> std::option::Option<PathBuf> {
+        self.current_directory
+            .ancestors()
+            .nth(1)
+            .map(|path| path.to_path_buf())
+    }
+
+    // Display
 
     fn show_breadcrumbs(&self) {
         println!("{}\r", self.current_directory.display());
@@ -85,4 +124,14 @@ impl App {
         }
         self.debug_messages.push(message.as_ref().to_owned());
     }
+}
+
+fn directory_contents(path: &PathBuf) -> Vec<String> {
+    WalkDir::new(path)
+        .max_depth(1)
+        .min_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .map(|entry| entry.file_name().to_string_lossy().into_owned())
+        .collect()
 }
